4655b4cbfcbcfd3a68515013292d09b034174747
cagy6464 <cagy6464@gmail.com>
2017-02-11 18:49:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne - {
+   title: 'Article One | Cagy6464',
+   heading: 'Article One',
+   date: 'Feb 5, 2017',
+   content:`
+            <p>
+               This is cagy6464s first content for article. 
+                Michael Faraday was an English scientist . 
+                His main discoveries include the principles underlying electromagnetic induction, diamagnetism and electrolysis.
+                The Faradays law of induction is named after him.
+
+                His main discoveries include the principles underlying electromagnetic induction, diamagnetism and electrolysis.
+                 Albert Einstein kept a picture of Faraday on his study wall, alongside pictures of Isaac Newton and James Clerk Maxwell.
+             </p> `
+   };
+
+function createTemplate (data) {
+var title = data.title;
+var date  = data.date;
+var heading = data.heading;
+var content = data.content;
+}
+
+
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name ="viewport" content ="width devide=width,intial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+             ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>
+
+return htmlTemplate;
+`;
+
 app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res){
